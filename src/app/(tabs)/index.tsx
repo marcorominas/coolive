b70386622145ae9485d746f4693648091e02c0e3
Mar corominas <marcorominasgratacos@gmail>
2025-05-18 11:09:42 -0700

    path aliases

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
-
+import Button from '@/components/Button';
 
 export default function HomeScreen() {
   return (
-    <View >
-      <Text className='text-3xl font-bold text-orange-500'>Feed</Text>
-
+    <View className='flex-1 p-4'>
+      <Text className='text-3xl font-bold text-white'>Tasks to do</Text>
+      <Button />
       <StatusBar style="auto" />
 
     </View>
---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import { Pressable, Text} from 'react-native';
+
+export default function Button(){
+    return  (
+        <Pressable>
+            <Text className="text-white">Click me</Text>
+        </Pressable>
+
+    );
+   
+} 
